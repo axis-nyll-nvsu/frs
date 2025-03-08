a97dab4c223e4aad4fe49239fd066e2b9ef17d31
Vernyll Jan P. Asis <asisvjp.nvsu@gmail.com>
2025-03-08 13:46:00 +0800

    [refactor] Remove environment variables from config.

--- config/config.php
+++ config/config.php
@@ -5,19 +5,31 @@
  * Author: 
  * Modified: 11-23-2024
  */
+require '../vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+// Load the .env file from the parent folder
+$dotenv = Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
 
 class Connection {
-	private $servername = "localhost";
-	private $username = "root";
-	private $password = "";
-	private $dbname = "frs";
-	private $dsn;
+	private $dbHost;
+	private $dbUser;
+	private $dbPass;
+	private $dbUser;
 	private $pdo;
+
+    public function __construct() {
+        $this->dbHost = $_ENV['DB_HOST'];
+        $this->dbUser = $_ENV['DB_USER'];
+        $this->dbPass = $_ENV['DB_PASS'];
+        $this->dbName = $_ENV['DB_NAME'];
+    }
 
 	public function conn() {
 		try {
-			$this->dsn = "mysql:host=" . $this->servername . ";dbname=" . $this->dbname;
-			$this->pdo = new PDO($this->dsn, $this->username, $this->password);
+			$this->pdo = new PDO("mysql:host=" . $this->dbHost . ";dbname=" . $this->dbName, $this->dbUser, $this->dbPass);
 			$this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			$this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 			return $this->pdo;			
