209e99f9496855ede1ab02341e1934f81a6c4936
Charlene B. Dela Cruz <charlenedelacruz09@gmail.com>
2025-04-18 12:58:29 +0800

    [fix] Adding of new account

--- admin/accounts.php
+++ admin/accounts.php
@@ -53,7 +53,7 @@ $accounts = $account->getData();
                     <div class="col-xs-12">
                         <div class="box">
                             <div class="box-header with-border">
-                                <a href="#addnew" data-toggle="modal" class="btn btn-sm btn-flat axis-btn-green">Add Account</a>
+                                <a href="#add" data-toggle="modal" class="btn btn-sm btn-flat axis-btn-green">Add Account</a>
                             </div>
                             <div class="box-body table-responsive">
                                 <table id="example1" class="table table-bordered">
--- admin/index.php
+++ admin/index.php
@@ -46,7 +46,7 @@ date_default_timezone_set('Asia/Manila');
                                     <input id="password-field" type="password" name="password" placeholder="Password" class="rounded w-100 h-100 border-0 bg-transparent" style="outline: none;">
                                 </div>
                             </div>
-                            <p><a href="forgotpassword.php" class="axis-link">Forgot Password?</a></p>
+                            <p style = "font-size: 12px;">Forgot Password? Contact your system administrator.</p>
                             <div class="form-group">
                                 <div class="form-control p-0 border-0">
                                     <input type="submit" name="login" class="signin btn-lg text-white w-100 border-0" value="Login" style="outline: none;background-color: #00693e;border-radius: 8px;font-size: 1rem;" onclick="lsRememberMe()">
